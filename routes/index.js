492a695e909b9c648c55716344e813a32b95c101
Muzucode <seanhickey4516@gmail.com>
2023-10-21 12:53:23 -0400

    Add users route

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,14 @@ router.get('/', (req, res) => {
     res.render('index');
 });
 
+/**
+ * @returns User
+ */
+router.get('/users/:userName', (req, res) => {
+    console.log('Fetching user' + req.params.userName)
+    // TODO: Implement fetch by 'Name' of user from Users collection
+
+    // Return of type User
+})
+
 module.exports = router
